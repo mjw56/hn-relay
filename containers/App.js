3140a346e0ad11fd446a1c215f1e5cc20d930269
Mike Wilcox <mwilcox56@gmail.com>
2015-10-17 20:54:40 -0400

    initial commit

---
+++ containers/App.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {};
+
+export {App}
